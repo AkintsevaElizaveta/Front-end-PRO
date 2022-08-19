fc0b18d9fe8abd66efd83dda7064e45a93b5752f
Елизавета Акинцева <akincevae@gmail.com>
2022-08-19 13:29:50 +0300

    changed Validate function and added Error function.

--- 23 - Nodejs/src/calculator.js
+++ 23 - Nodejs/src/calculator.js
@@ -8,24 +8,35 @@ const divModule = require('./div.js')
 module.exports.add = function add(a, b){
     if (validateNumber(a) && validateNumber(b))
         return addModule.add(a, b);
-
+    else
+        return showError();
 };
 
 module.exports.sub = function sub(a, b){
     if (validateNumber(a) && validateNumber(b))
         return subModule.sub(a, b)
+    else
+        return showError();
 };
 
 module.exports.mult = function mult(a, b){
     if (validateNumber(a) && validateNumber(b))
         return multModule.mult(a, b)
+    else
+        return showError();
 };
 
 module.exports.div =function div(a, b){
-    if (validateNumber(a) && validateNumber(b))
+    if (validateNumber(a) && validateNumber(b) && b !== 0)
         return divModule.div(a, b)
+    else
+        return showError();
 };
 
 function validateNumber(number){
     return !isNaN(number);
+}
+
+function showError(){
+    return 'error'
 }
