95264ca34c119d60a24b30554df40a8c1248f4f1
Akintseva Elizaveta <105868447+AkintsevaElizaveta@users.noreply.github.com>
2022-10-06 10:13:38 +0300

    Delete index.js

--- 33hw_gallery_react-router/src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import users from './users';
-import albums from './albums';
-import album from './album';
-
-
-export default combineReducers({
-    users, albums, album
-})
